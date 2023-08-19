742573290c1bf22c2d64353986e5b513ff758054
DiogoS77 <diogo.messi.18@hotmail.com>
2023-08-19 20:31:08 +0100

    reworked database.js and added missing code

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,21 +1,54 @@
-import { openDB } from 'idb';
+import {openDB} from "idb";
 
-const initdb = async () =>
-  openDB('jate', 1, {
+const initializeDatabase = async () =>
+  openDB("uniqueDB", 1, {
     upgrade(db) {
-      if (db.objectStoreNames.contains('jate')) {
-        console.log('jate database already exists');
+      if (db.objectStoreNames.contains("uniqueStore")) {
+        console.log("Unique database already exists");
         return;
       }
-      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
-      console.log('jate database created');
+      db.createObjectStore("uniqueStore", {keyPath: "id", autoIncrement: true});
+      console.log("Unique database created");
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const addToUniqueDb = async (content) => {
+  console.log("Adding to the unique database");
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+  const uniqueDb = await openDB("uniqueDB", 1);
+  const tx = uniqueDb.transaction("uniqueStore", "readwrite");
+  const store = tx.objectStore("uniqueStore");
 
-initdb();
+  const request = store.put({id: 1, content: content});
+
+  const result = await request;
+  console.log("🚀 - Data added to the unique database", result);
+};
+
+export const retrieveFromUniqueDb = async () => {
+  console.log("Retrieving from the unique database");
+
+  const uniqueDb = await openDB("uniqueDB", 1);
+  const tx = uniqueDb.transaction("uniqueStore", "readonly");
+  const store = tx.objectStore("uniqueStore");
+
+  const request = store.get(1);
+
+  const result = await request;
+  return result?.content;
+};
+
+export const removeFromUniqueDb = async (id) => {
+  console.log("Removing from the unique database", id);
+
+  const uniqueDb = await openDB("uniqueDB", 1);
+  const tx = uniqueDb.transaction("uniqueStore", "readwrite");
+  const store = tx.objectStore("uniqueStore");
+
+  const request = store.delete(id);
+
+  const result = await request;
+  console.log("Data removed from the unique database", result);
+};
+
+initializeDatabase();
